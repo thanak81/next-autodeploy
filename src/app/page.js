8ad424c424a8c6755594b6ac95ccbfd508f873bd
Thanak Mech <thanakmech@gmail.com>
2024-09-18 13:41:05 +0700

    add more content

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>Je m'applle Mech Thanak. Je suis 20 ans. J'habite a Phnom Penh et je suis eleve. J'etudie en Information De Technology a Universare en Royal Phnom Penh au Cambodge. Mais j'ai fini mon diplome cette annee.Je suis tres heureux</div>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
